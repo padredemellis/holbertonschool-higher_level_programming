573e204f4d5f7dbf9464286cc12e1b6e4247c4dc
padredemellis <10656@holbertonstudents.com>
2025-05-13 03:03:50 +0000

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for n in range(10):
-    for x in range(10):
-        if x > n:
-            if n == 8 and x == 9:
-                print("89")
-            else:
-                print("{}{}, ".format(n, x), end="")
-
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
