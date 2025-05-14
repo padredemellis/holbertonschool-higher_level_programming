e5bb3a0ff4d5dcde91f63b933d7ea6793bb7e827
padredemellis <10656@holbertonstudents.com>
2025-05-14 01:30:59 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("{} arguments".format(num_args))
+        print("{} arguments.".format(num_args))
     elif num_args == 1:
         print("{} argument:\n{}: {}".format(num_args, num_args, sys.argv[1]))
     else:
