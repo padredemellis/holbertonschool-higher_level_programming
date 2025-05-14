881784be067d5672b9fabbbfbe404221800c7c2d
padredemellis <10656@holbertonstudents.com>
2025-05-14 01:14:57 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{} arguments".format(num_args))
+    elif num_args == 1:
+        print("{} argument:\n {}: {}".format(num_args, num_args, sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
