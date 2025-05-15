8b0a28acec6ecb631f0b177e2f3d438ff0625b93
root <root@emadev.>
2025-05-15 13:16:25 -0300

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence == 0:
-        return None
-    else:
-        return (len(sentence), sentence[0])
+    if not sentence:
+        return (0, None)
+    return (len(sentence), sentence[0])
