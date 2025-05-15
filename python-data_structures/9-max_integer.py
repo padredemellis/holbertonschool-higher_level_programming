1236e04951ba25eba1170ce7edba6fc436b48542
root <root@emadev.>
2025-05-15 13:32:01 -0300

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,8 +4,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     mayor = my_list[0]
-    for n in my_list:
+    for n in my_list[1:]:
         if n > mayor:
             mayor = n
             
-            return mayor
+    return mayor
