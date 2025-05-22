f978b62c76580532380689e7828bb1cdbba18930
root <root@emadev.>
2025-05-22 13:23:54 -0300

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module is my first class
+"""
 class Square:
+    """
+    This class represents a square.
+    The class is currently empty but will be expanded in future exercises
+    to include attributes and methods related to squares.
+    """
     def __init__(self):
         pass
