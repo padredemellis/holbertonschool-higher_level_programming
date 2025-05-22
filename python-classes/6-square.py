788c9242acf67263191537cabc4a17b0da41e1f7
root <root@emadev.>
2025-05-22 19:58:13 -0300

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,13 +8,15 @@ class Square:
     """
     This class represents a square.
     """
-    def __init__(self, size=0, position=(0,0)):
+
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
     @property
     def size(self):
         return self.__size
+
     @property
     def position(self):
         return self._position
@@ -26,14 +28,14 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or len(value) != 2
-            or not all(isinstance(num, int) for num in value)):
+                or not all(isinstance(num, int) for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self._position = value
-    
+
     def area(self):
         return (self.size * self.size)
 
@@ -41,9 +43,9 @@ class Square:
         if self.size == 0:
             print()
             return
-        
+
         for _ in range(self._position[1]):
             print()
-        
+
         for _ in range(self.size):
             print(" " * self.position[0] + "#" * self.size)
