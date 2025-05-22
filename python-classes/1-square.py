19f9eef2e83352e4ee5e609446bf89c741dd3312
root <root@emadev.>
2025-05-22 13:38:03 -0300

    1.1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
