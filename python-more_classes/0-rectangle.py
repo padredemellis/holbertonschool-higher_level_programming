59d95af953014482d6416b0bdac3fa99b1ba686d
padredemellis <10656@holbertonstudents.com>
2025-05-25 12:38:56 -0300

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module is my first Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+    The class is currently empty but will be expanded in future exercises
+    to include attributes and methods related to squares.
+    """
+
+    def __init__(self):
+        pass
