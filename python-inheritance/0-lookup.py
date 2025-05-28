533ef18e5c1a159b216301abaf76867726588bfe
root <root@emadev.>
2025-05-28 06:55:12 -0300

    0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+In this module we learn how to use the dir() method
+'''
+def lookup(obj):
+    '''
+    The lookup(obj) function returns a list of methods and attributes of an object.
+    '''
+    return [m for m in dir(obj)]
