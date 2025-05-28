aafad6adce63bc2b23ff70f9a9e3a54a40eaea77
root <root@emadev.>
2025-05-28 10:29:24 -0300

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,15 +4,15 @@ We create an inheritor class
 '''
 
 class MyList(list):
-    '''My list inheritor of list'''
-    
+    '''
+    My list inheritor of list
+    '''
     
     def print_sorted(self):
-        
-        
         """
         Prints the list in ascending sorted order.
         Assumes all elements are of type int.
         Does not modify the original list.
         """
+        
         print(sorted(self))
