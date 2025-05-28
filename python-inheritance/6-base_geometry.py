45b8f054f906e9a4a5fc440d26a9713b1a05b321
root <root@emadev.>
2025-05-28 13:47:05 -0300

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+    Aquí se edificará algo
+    """
+    def area(self):
+        ''' Area vacia'''
+        raise Exception("area() is not implemented")
