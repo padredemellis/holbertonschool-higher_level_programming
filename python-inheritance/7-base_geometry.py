26d95a44be10c67b463affe9fa8aad0c576d5552
root <root@emadev.>
2025-05-28 14:22:00 -0300

    7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 6-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+    Aquí se edificará algo
+    """
+    def area(self):
+        """Raise an exception for unimplemented area."""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self,name,value):
+        """Validate a value as a positive integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
