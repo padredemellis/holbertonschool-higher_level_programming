666b5398ca358c1eba43f23fce67b47f267e03ab
root <root@emadev.>
2025-05-28 13:21:09 -0300

    4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,5 +7,6 @@ of a class that inherited
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of a class that inherited from."""
+    """Check if an object is an instance
+    of a class that inherited from."""
     return isinstance(obj,a_class) and type(obj) is not a_class
