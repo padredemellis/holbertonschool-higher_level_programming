32a527e6a7a6557ddd1e649b019929133325103c
root <root@emadev.>
2025-05-29 12:53:15 -0300

    4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 Write a function that returns True if the object is an instance
-of a class that inherited 
+of a class that inherited
 (directly or indirectly) from the specified class ; otherwise False.
 '''
 
@@ -9,4 +9,4 @@ of a class that inherited
 def inherits_from(obj, a_class):
     """Check if an object is an instance
     of a class that inherited from."""
-    return isinstance(obj,a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
