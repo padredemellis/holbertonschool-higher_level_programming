ed59c24ae19da6ee477d41d9e4537eb6bd131e31
root <root@emadev.>
2025-06-04 15:23:03 -0300

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 '''Write a function that reads a text file (UTF8) and prints it to stdout:'''
 
 
-def read_file(filename = ""):
+def read_file(filename=""):
     """This function read a file"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end = "")
+        print(f.read(), end="")
