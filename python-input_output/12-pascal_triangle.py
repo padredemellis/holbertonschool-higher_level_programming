ade6c00b728f72bbd5f04795c9406d6a1b56e95d
root <root@emadev.>
2025-06-06 18:39:04 -0300

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''Create a function def pascal_triangle(n):
+that returns a list of lists of integers
+representing the Pascal’s triangle of n:'''
+
 
 def pascal_triangle(n):
     """Devuelve el Triángulo de Pascal de n filas como lista de listas de enteros."""
