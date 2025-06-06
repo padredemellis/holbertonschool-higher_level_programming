bf4db2f33ad77fd0858c9cf0e4e7e82f47d4f85c
root <root@emadev.>
2025-06-06 09:39:16 -0300

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-Write a function that returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization of an object:
+Write a function that returns the dictionary
+description with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object:
 """
 
 def class_to_json(obj):
-    """Retorna la descripción del diccionario para serialización JSON de un objeto.
+    """Retorna la descripción del diccionario
+    para serialización JSON de un objeto.
     
     Args:
         obj: Instancia de una clase con atributos serializables.
