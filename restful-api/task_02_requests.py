8e02f176bf6bf97394254e75c5d9c07381ed17ab
root <root@emadev.>
2025-06-12 18:13:16 -0300

    restful2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,19 +2,22 @@
 """Consumir y procesar datos de una API con Python"""
 
 import requests
-
+import csv
 
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
-
         for post in posts:
             print(post['title'])
-    else:
-        print(f"Error en la peticion: {response.status_code}")
 
 def fetch_and_save_posts():
-    pass
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        structured_data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+        with open("posts.csv", "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(structured_data)
