7bd81b1a09b61cc79bd91be2c897b4eba7d32ffd
padredemellis <10656@holbertonstudents.com>
2025-06-24 22:05:08 -0300

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,16 @@
--- full creation
+-- Creates table second_table and inserts multiple rows
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+USE hbtn_test_db_9;
+
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score) VALUES
-(1, 'Jhon', 10)
-(2, 'Alex', 3)
-(3, 'Bob', 14)
-(4, 'George', 8);
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
