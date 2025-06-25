ddcc281662595ef66d25a16243d5ed323b0f3ef0
padredemellis <10656@holbertonstudents.com>
2025-06-24 21:50:51 -0300

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- full creation
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256),
     score INT
 );
 INSERT INTO second_table (id, name, score) VALUES
-(1, "Jhon", 10)
-(2, "Alex", 3)
-(3, "Bob", 14)
-(4, "George", 8);
+(1, 'Jhon', 10)
+(2, 'Alex', 3)
+(3, 'Bob', 14)
+(4, 'George', 8);
