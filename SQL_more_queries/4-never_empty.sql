e0a6bd609ecd1da90418adad667f4aa4f4804fce
root <root@emadev.>
2025-06-28 17:05:24 -0300

    4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,8 @@
--- Never empty
-CRESATE TABLE IF NOT EXISTS id_not_null(
-    ID INT NOT NULL DEFAULT 1,
+-- Asegura la existencia de la base de datos específica
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
+
+-- Crea la tabla en la base de datos correcta
+CREATE TABLE IF NOT EXISTS hbtn_test_db_4.id_not_null (
+    id INT NOT NULL DEFAULT 1,
     name VARCHAR(256)
 );
