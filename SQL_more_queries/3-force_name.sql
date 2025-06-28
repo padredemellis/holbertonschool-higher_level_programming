36978d8906791d8608a84bc53a06910156704b5a
root <root@emadev.>
2025-06-28 16:50:04 -0300

    3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,6 @@
 -- 3-force_name.sql
-USE `$1`;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
+USE hbtn_test_db_3;
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
