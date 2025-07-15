8408cc2d96ff49c42215a27ce322664d314980cc
padredemellis <10656@holbertonstudents.com>
2025-07-14 21:01:48 -0300

    2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let args = process.argv.slice(2)
+if (!args.length){
+    console.log('No argument')
+}
+else if (args.length === 1){
+    console.log('Argument found')
+}
+else{
+    console.log('Arguments found')
+}
