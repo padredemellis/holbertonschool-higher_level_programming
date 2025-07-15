7697da05e63ef1a898a6737005b11bb9c181befd
padredemellis <10656@holbertonstudents.com>
2025-07-14 23:28:39 -0300

    9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,8 +6,4 @@ function add (a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Arguments must be numbers');
-} else {
-  console.log(add(num1, num2));
-}
+console.log(add(num1, num2));
